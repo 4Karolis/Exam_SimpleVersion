a6b88a276bc7b11357f5b06540d7013a606a7030
4Karolis <4karolis.t@gmail.com>
2022-07-07 19:21:10 +0300

    Created Entities

---
+++ Exam_SimpleVersion/Exam_SimpleVersion/Entities/Lecture.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_SimpleVersion.Entities
+{
+    public class Lecture
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Department> Departments { get; set; }
+        public List<Student> Students { get; set; }
+
+        public Lecture(string name)
+        {
+            Name = name;
+            Departments = new List<Department>();
+            Students = new List<Student>();
+        }
+        //public Lecture(string name, Department department)
+        //{
+        //    Name = name;
+        //    Departments = department;
+        //    Departments = new List<Department>();
+        //    Students = new List<Student>();
+        //}
+        //public Lecture(string name, Department department)
+        //{
+        //    Name = name;
+        //    LectureDepartments = LectureDepartments.Add(department);
+        //}
+        private Lecture() { }
+    }
+}
---
+++ Exam_SimpleVersion/Exam_SimpleVersion/Entities/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_SimpleVersion.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public List<Lecture> Lectures { get; set; }
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+        public Student(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Lectures = new List<Lecture>();
+            //StudentDepartment = new Department();
+            //add StudentDepartment = new Department();?
+            //NEW DEPO missing!
+        }
+        public Student(string firstName, string lastName, DateTime dateOfBirth, Department department)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Lectures = new List<Lecture>();
+            Department = department;
+        }
+        private Student() { }
+    }
+}
